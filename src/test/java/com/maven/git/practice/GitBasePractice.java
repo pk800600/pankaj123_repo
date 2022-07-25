1509ec66c4931dbf4c0318e880ee58d6ff7cd395
pk800600 <pankaj800600@gmail.com>
2022-07-25 12:53:51 -0400

    Fourth commit

---
+++ src/test/java/com/maven/git/practice/GitBasePractice.java
@@ -0,0 +1,14 @@
+package com.maven.git.practice;
+
+public class GitBasePractice {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Git prectice");
+		System.out.println("git prectice2");
+	}
+	
+	//close
+	//good morning
+
+}
