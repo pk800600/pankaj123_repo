d0858f711a35bc36049499411a0ef5bec93c8aa7
pk800600 <pankaj800600@gmail.com>
2022-07-25 13:06:10 -0400

    Six commit

---
+++ src/test/java/com/maven/git/practice/GitPractice2Test.java
@@ -0,0 +1,12 @@
+package com.maven.git.practice;
+
+public class GitPractice2Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Good morning");
+		
+		System.out.println("Practice 1");
+	}
+
+}
